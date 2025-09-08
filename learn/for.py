cbe85a56c53658707bb13c31525eaa630545a70a
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-09-09 02:31:49 +0300

    add learning for.py file

---
+++ learn/for.py
@@ -0,0 +1,9 @@
+# #####################################################################################################################################################
+# ---------- For Loops ----------
+#              Syntax
+# for item in iterable:
+#   Whatever you want (Body)
+# continue  : it breaks the current iteration and continue the rest iteration of the loop
+# break     : it breaks the loop
+# pass      : it passes this block of code. e.g., if you want to skip the funciton of for loop just write 'pass' in the Body of the loop                               #
+# #####################################################################################################################################################
