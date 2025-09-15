d0b85b2f4d20241a758d36f940401f5577e734bc
Kareem Ashraf <ashrafkareem825@gmail.com>
2025-09-16 02:10:05 +0300

    edit salary.py for better readability

--- APPs/salary.py
+++ APPs/salary.py
@@ -8,7 +8,7 @@ percent = float(input("Enter the addition percent per year: "))
 tot = first * 1000
 for year in range(1, 60): 
     tot += 12 * month
-    print(f"Year({year}): {month:,.3f}")
+    print(f"Year({str(year).zfill(2)}): {month:,.0f}")
     month = month + month * percent/100.
-print(f"Total money payed over 59 years is: {tot:,.3f}")
-print(f"The average payed per one year is: {tot/59:,.3f}")
+print(f"Total money payed over 59 years is: {tot:,.0f}")
+print(f"The average payed per one year is: {tot/59:,.0f}")
